7695b95cfe452dfd9743712c9914bc42dad72303
Martha <marthasibiya25@gmail.com>
2026-07-14 14:09:44 +0200

    save

--- cse340-service-network/models/categories.js
+++ cse340-service-network/models/categories.js
@@ -1,18 +1,19 @@
-app.get("/categories", (req, res) => {
-    const categories = [
-        {
-            category_name: "Community Service"
-        },
-        {
-            category_name: "Environmental"
-        },
-        {
-            category_name: "Education"
-        }
-    ];
+import pool from "../database/index.js";
 
-    res.render("categories", {
-        title: "Service Project Categories",
-        categories
-    });
-});
+async function getCategories() {
+    try {
+        const result = await pool.query(
+            "SELECT * FROM category ORDER BY category_name"
+        );
+
+        return result.rows;
+
+    } catch (error) {
+        console.error("Error getting categories:", error);
+        return [];
+    }
+}
+
+export default {
+    getCategories
+};
