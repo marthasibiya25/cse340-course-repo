828e9ebcbc84d8bd4a6b5b8dc5554151cd8d1e6b
Martha <marthasibiya25@gmail.com>
2026-08-01 00:25:46 +0200

    save all

--- cse340-service-network/controllers/projectController.js
+++ cse340-service-network/controllers/projectController.js
@@ -1,13 +1,17 @@
 import projectsModel from "../models/projects.js";
 
 
+// Display project details page
 const buildProjectDetail = async (req, res) => {
 
     const id = req.params.id;
 
+
     const project = await projectsModel.getProjectById(id);
 
+
     const categories = await projectsModel.getCategoriesByProjectId(id);
+
 
     res.render("project-detail", {
         title: project.project_name,
@@ -18,6 +22,106 @@ const buildProjectDetail = async (req, res) => {
 };
 
 
+
+// Display assign categories page
+const buildAssignCategories = async (req, res) => {
+
+    const id = req.params.id;
+
+
+    const project = await projectsModel.getProjectById(id);
+
+
+    const allCategories = await projectsModel.getAllCategories();
+
+
+    const projectCategories = await projectsModel.getCategoriesByProjectId(id);
+
+
+
+    const assignedCategoryIds = projectCategories.map(
+        category => category.category_id
+    );
+
+
+    res.render("assign-categories", {
+
+        title: "Assign Categories",
+
+        project,
+
+        allCategories,
+
+        assignedCategoryIds,
+
+        error: null
+
+    });
+
+};
+
+
+
+// Process category assignments
+const updateProjectCategories = async (req, res) => {
+
+    const id = req.params.id;
+
+
+    let { category_ids } = req.body;
+
+
+
+    // If only one checkbox is selected
+    if (!Array.isArray(category_ids)) {
+
+        category_ids = category_ids
+            ? [category_ids]
+            : [];
+
+    }
+
+
+
+    try {
+
+        await projectsModel.updateProjectCategories(
+            id,
+            category_ids
+        );
+
+
+        req.flash(
+            "success",
+            "Project categories updated successfully!"
+        );
+
+
+        res.redirect(`/project/${id}`);
+
+
+
+    } catch (error) {
+
+        console.error(error);
+
+
+        res.render("assign-categories", {
+
+            title: "Assign Categories",
+
+            error: "Unable to update project categories."
+
+        });
+
+    }
+
+};
+
+
+
 export default {
-    buildProjectDetail
+    buildProjectDetail,
+    buildAssignCategories,
+    updateProjectCategories
 };
--- cse340-service-network/routes/projectRoutes.js
+++ cse340-service-network/routes/projectRoutes.js
@@ -1,13 +1,27 @@
 import express from "express";
 import projectController from "../controllers/projectController.js";
 
-
 const router = express.Router();
 
 
+// Project details page
 router.get(
     "/project/:id",
     projectController.buildProjectDetail
+);
+
+
+// Assign categories page
+router.get(
+    "/assign-categories/:id",
+    projectController.buildAssignCategories
+);
+
+
+// Save assigned categories
+router.post(
+    "/assign-categories/:id",
+    projectController.updateProjectCategories
 );
 
 
