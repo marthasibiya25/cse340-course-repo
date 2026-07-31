9f4e45e4fab62f1004fbbefb5b0288e8d6f554e7
Martha <marthasibiya25@gmail.com>
2026-07-31 23:20:23 +0200

    save

--- cse340-service-network/server.js
+++ cse340-service-network/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import session from "express-session";
+import flash from "connect-flash";
 
 import categoryRoutes from "./routes/categoryRoutes.js";
 import projectRoutes from "./routes/projectRoutes.js";
@@ -21,6 +23,33 @@ app.set("view engine", "ejs");
 
 // Static files
 app.use(express.static("public"));
+
+
+// Form data parser
+app.use(express.urlencoded({ extended: true }));
+
+
+// Session and flash messages
+app.use(
+    session({
+        secret: "service-network-secret",
+        resave: false,
+        saveUninitialized: true
+    })
+);
+
+
+app.use(flash());
+
+
+// Make flash messages available to EJS
+app.use((req, res, next) => {
+
+    res.locals.messages = req.flash();
+
+    next();
+
+});
 
 
 // Home
