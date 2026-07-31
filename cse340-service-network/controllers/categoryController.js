3ca8de8e0403cb191dc0644993b0c5f9742f359d
Martha <marthasibiya25@gmail.com>
2026-08-01 00:02:48 +0200

    save

--- cse340-service-network/controllers/categoryController.js
+++ cse340-service-network/controllers/categoryController.js
@@ -1,7 +1,7 @@
 import categoriesModel from "../models/categories.js";
 
 
-// Category detail page
+// Display category details page
 const buildCategoryDetail = async (req, res) => {
 
     const id = req.params.id;
@@ -20,7 +20,8 @@ const buildCategoryDetail = async (req, res) => {
 };
 
 
-// Show create category form
+
+// Display create category form
 const buildNewCategory = async (req, res) => {
 
     res.render("new-category", {
@@ -31,12 +32,14 @@ const buildNewCategory = async (req, res) => {
 };
 
 
+
 // Process create category form
 const createCategory = async (req, res) => {
 
     const { category_name } = req.body;
 
 
+    // Server-side validation
     if (
         !category_name ||
         category_name.length < 3 ||
@@ -80,8 +83,94 @@ const createCategory = async (req, res) => {
 };
 
 
+
+// Display edit category form
+const buildEditCategory = async (req, res) => {
+
+    const id = req.params.id;
+
+
+    const category = await categoriesModel.getCategoryById(id);
+
+
+    res.render("edit-category", {
+        title: "Edit Category",
+        category,
+        error: null
+    });
+
+};
+
+
+
+// Process edit category form
+const updateCategory = async (req, res) => {
+
+    const id = req.params.id;
+
+    const { category_name } = req.body;
+
+
+    // Server-side validation
+    if (
+        !category_name ||
+        category_name.length < 3 ||
+        category_name.length > 100
+    ) {
+
+        const category = await categoriesModel.getCategoryById(id);
+
+
+        return res.render("edit-category", {
+            title: "Edit Category",
+            category,
+            error: "Category name must be between 3 and 100 characters."
+        });
+
+    }
+
+
+    try {
+
+        await categoriesModel.updateCategory(
+            id,
+            category_name
+        );
+
+
+        req.flash(
+            "success",
+            "Category updated successfully!"
+        );
+
+
+        res.redirect("/categories");
+
+
+    } catch (error) {
+
+        console.error(error);
+
+
+        const category = await categoriesModel.getCategoryById(id);
+
+
+        res.render("edit-category", {
+            title: "Edit Category",
+            category,
+            error: "This category already exists. Please choose another name."
+        });
+
+    }
+
+};
+
+
+
 export default {
     buildCategoryDetail,
     buildNewCategory,
-    createCategory
+    createCategory,
+    buildEditCategory,
+    updateCategory
 };
